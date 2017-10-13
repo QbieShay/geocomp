5e0a039fb3c9fc9674f3db425a1fdcb2df4a14e8
QbieShay <cislaghi.ilaria@gmail.com>
2017-10-13 16:12:56 +0200

    popup implemented

---
+++ ui/gameui/EndGamePopup.gd
@@ -0,0 +1,25 @@
+extends Popup
+var Utils = preload("res://Utils.gd")
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+func _ready():
+	# Called every time the node is added to the scene.
+	# Initialization here
+	pass
+
+func set_score(score):
+	get_node("ScoreLabel").set_text("YOUR SCORE:"+str(score))
+
+func reset():
+	hide()
+	get_tree().get_root().find_node("GameUI")._on_ResetButton_pressed()
+
+func next():
+	var lvl = get_node(Utils.name_from_root("Level"))
+	if(lvl.level_num >=5):
+		pass
+	else:
+		lvl.load_level(lvl.level_num + 1)
